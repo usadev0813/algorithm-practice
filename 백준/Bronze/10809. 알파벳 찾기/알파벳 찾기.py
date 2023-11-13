763fd2c76cfd7202d8d95345093edaea4ea5411e
이정호 <84367108+jeonghoHub@users.noreply.github.com>
2023-11-14 07:58:28 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,6 @@
+s = input()
+target = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+          'w', 'x', 'y', 'z']
+
+for i in range(len(target)):
+    print(s.find(target[i]), end=' ')

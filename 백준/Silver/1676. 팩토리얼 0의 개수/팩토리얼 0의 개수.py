94b611a60902e7956d74f1a533e9013604f71e1f
이정호 <84367108+jeonghoHub@users.noreply.github.com>
2023-11-16 08:41:08 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
@@ -5,9 +5,7 @@ fac_result = 1
 for i in range(1, N + 1):
     fac_result *= i # N팩토리얼 값을 구한다
 
-reverse_str_fac_result = list(str(fac_result)) # N팩토리얼 값을 문자열 리스트로 변환한다.
-
-for str_fac_num in reversed(reverse_str_fac_result):
+for str_fac_num in reversed(str(fac_result)):
     if str_fac_num == '0': result += 1
     else: break
 
